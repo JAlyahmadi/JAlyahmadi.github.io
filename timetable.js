d591421212c3bf906731e517a793e41880db8407
JAlyahmadi <89706055+JAlyahmadi@users.noreply.github.com>
2021-10-13 19:57:20 +0400

    Update timetable.js

--- timetable.js
+++ timetable.js
@@ -25,16 +25,25 @@
 var timetableText = `
 
 
+p2s1|SFR ICA1|03/10/2021|08:00|1 hour, 12.5% weightage
+p2s1|RIP ICA1|10/10/2021|08:00|18% weightage
+p2s1|Growth ICA|24/10/2021|09:00|40% weightage
+p2s1|Hospital ICA|24/10/2021|12:00|50 minutes
+p2s1|SFR ICA2|07/11/2021|08:00|1 hour, 12.5% weightage
+p2s1|RIP ICA2|21/11/2021|08:00|18% weightage
+p2s1|Hospital SPSS Practical|28/11/2021|12:00|50 minutes
+p2s1|SFR OSPE|13/12/2021|13:00|3 hours, 13% weightage
+p2s1|Growth Final|27/12/2021|00:00|Time not confirmed
 
-p1|Temporary Placeholder|06/08/2021|08:00|1.5 Hours
-p1|Temporary Placeholder|15/08/2021|08:00|1.5 Hours
-p1|Temporary Placeholder|17/08/2021|13:30|20 Minutes
-p2s1|Temporary Placeholder|07/08/2021|08:00|1.5 Hours
-p2s1|Temporary Placeholder|16/08/2021|08:00|1.5 Hours
-p2s1|Temporary Placeholder|23/08/2021|09:00|1 Hour
-P2s1|Temporary Placeholder|30/08/2021|08:00|40 Minutes
-p2s3|Temporary Placeholder|05/08/2021|08:00|1.5 Hours
-p2s3|Temporary Placeholder|14/08/2021|08:00|1.5 Hours
+p2s3|Uro-reproductive ICA1|12/10/2021|09:00|No extra information
+p2s3|Alimentary ICA1|17/10/2021|08:00|No extra information
+p2s3|Clinical Nutrition ICA|28/10/2021|00:00|Time not specified
+p2s3|Integrated II ICA|01/11/2021|00:00|Time not specified
+p2s3|Uro-reproductive ICA2|14/11/2021|09:00|1 hour
+p2s3|Alimentary ICA2|21/11/2021|09:00|1 hour
+p2s3|Uro-reproductive Spotter|05/12/2021|10:00|No extra information
+p2s3|Alimentary Spotter|13/12/2021|8:00|No extra information
+p2s3|Integrated II OSCE|20/12/2021|00:00|Date and time not specified
 
 
 `;
