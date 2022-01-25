98fe2ceecdbaa1f20385b6fb983feca7fdfd8964
JAlyahmadi <89706055+JAlyahmadi@users.noreply.github.com>
2022-01-25 22:33:46 +0400

    Update announcements.js

--- announcements.js
+++ announcements.js
@@ -10,7 +10,7 @@ var announcements = `
 
 <h2>Message to 2018 Batch Students</h2>
 <p>Lately, a lot of people have been asking me about integrated module 3 summaries. I am not ditching them; I am already making progress in them, though very slow progress. If you are in a hurry, you should study from the slides available until I am done. Summary making has been quite slow this semester, probably due to full on-campus teaching. I still wish to make summaries for every single thing, including tutorials and practicals, but expect them to come out later than what you might had been used to in previous semesters.</p>
-<p>I will try my absolute best to be fully done with all week 1 and week 2 content summary-wise by the end of this week (week 2), but no promises made, so bear with me please. I apologise for all inconveniences, I trying to do my best.</p>
+<p>I will try my absolute best to be fully done with all week 1 and week 2 content summary-wise by the end of this week (week 2), but no promises made, so bear with me please. I apologise for all inconveniences, I am trying to do my best.</p>
 <p>- Jihad Al-Yahmadi (25/01/2022, 22:30)</p>
 
 `;
