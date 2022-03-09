1d9619e804365eaaa8bb03525ae7c6b0a85e409a
HusainAlaswami <92455991+HusainAlaswami@users.noreply.github.com>
2022-03-09 05:22:37 +0400

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -31,6 +31,11 @@
 // DO NOT RENAME THIS FILE
 
 var changelogText = `
+09/03/2022 05:21
+{anki} Updated HNS to include L32
+{anki} I think I got the hang of the missing cards: I highly recommend doing anking cards on the basal ganglia. They are to the point and leave out all the useless fluff the professors at our college add
+Hus
+---
 08/03/2022 23:30
 {anki} Updated Endocrine system deck to include T2: Pituitary Apoplexy, T3: Hypothyroidism & T4: Hyperthyroidism.
 ---
