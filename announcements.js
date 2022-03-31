948f4a76681f86f02a5d69cc9b905e4db0308382
JAlyahmadi <89706055+JAlyahmadi@users.noreply.github.com>
2022-04-01 00:32:19 +0400

    Add files via upload

--- announcements.js
+++ announcements.js
@@ -5,11 +5,10 @@
 // PLEASE DO NOT EDIT ANYTHING ELSE IN THE CODE, INCLUDING THE ENDING `;
 // DO NOT RENAME THIS FILE
 
-var showAnnouncement = false;
+var showAnnouncement = true;
 var announcements = `
 
-<h2>For Students Studying for the IM3 ICA</h2>
-<p>If you are a phase 2 semester 4 student studying for the IM3 ICA and you rely on my summaries, please make sure you go through the objectives yourself and judge whether there is anything missing and needs to be covered on your own or not. Unlike last semester for IM2 when I did this task myself and made summaries for all missing objectives, I am too busy this semester to do this for IM3. I am so sorry, please go through the objectives and check for yourself. Best of luck in the exam :)</p>
-<p>- Jihad Al-Yahmadi</p>
+<h1 style='font-size: 90px; text-align: center; color: pink'>Happy April Fools!</h1>
+<p style="text-align: center">I'm sorry :D</p>
 
 `;
