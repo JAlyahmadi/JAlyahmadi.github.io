7dd814840b4596e63c98c1768212a05b19b9f108
JAlyahmadi <89706055+JAlyahmadi@users.noreply.github.com>
2022-04-03 15:10:45 +0400

    Add files via upload

--- announcements.js
+++ announcements.js
@@ -8,7 +8,10 @@
 var showAnnouncement = true;
 var announcements = `
 
-<h1 style='font-size: 90px; text-align: center; color: pink'>Happy April Fools!</h1>
-<p style="text-align: center">I'm sorry :D</p>
-
+<h2>Announcement to Phase 2 Semester 4 Students</h2>
+<br>
+<p><b style="color: var(--primary)">Locomotor practical summaries:</b> All 3 practical summaries are up, but please note that P1 and P2 only talk about joints, their movements, and the muscles responsible for those movements, but they have no pictures. Use them as a guide for your studies, and study the location of each of them on your own.</p>
+<p><b style="color: var(--primary)">HNS practical summaries:</b> I highly recommend you use them for your studies. Most of them have everything you need organised in 1-2 pages. If there is anything missing, I note it out first thing in the summary and give you links for further studies and practise.</p>
+<p><b style="color: var(--primary)">IM3 skills lab summaries:</b> I highly, highly, highly recommend you study them for the OSCE. The checklists on Moodle only have video links. My summaries transcribe those videos and have notes from the sessions themselves. They contain everything you need to know, and I have also attached the video links for the sake of completion.</p>
+<p>I wish you all the best of luck.<br>- Jihad Al-Yahmadi</p>
 `;
