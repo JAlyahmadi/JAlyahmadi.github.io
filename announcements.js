02875787861dcce4546ae1a768f2ce593523d1f1
JAlyahmadi <89706055+JAlyahmadi@users.noreply.github.com>
2022-05-01 22:51:05 +0400

    Update announcements.js

--- announcements.js
+++ announcements.js
@@ -8,8 +8,5 @@
 var showAnnouncement = true;
 var announcements = `
 
-<h2>For Phase 2 Sem 4 Students</h2>
-<p>Almost all summaries for HNS, locomotor and endocrine (except a few tutorial summaries) are done. I will work on the rest of the tutorials and the IM3 summaries shortly. I apologise for the long day.</p>
-<p>For the HNS spotter, all summaries for all practicals are ready. Please pay attention to what I write in the notes/disclaimer area. You may use the summary as a core study material, but I highly recommend you check the links I post at least once. Some are mandatory.</p>
-<p>- Jihad Al-Yahmadi</p>
+<img src="https://puu.sh/IXWCI/07ac6769b6.jpg" width="100%">
 `;
